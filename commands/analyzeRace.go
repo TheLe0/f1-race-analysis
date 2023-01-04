59487095618f20b4c6089b1e666f7c90553aac03
TheLe0 <leob.tosin@hotmail.com>
2023-01-03 21:44:23 -0300

    feat: Create racer data type

--- commands/analyzeRace.go
+++ commands/analyzeRace.go
@@ -4,4 +4,15 @@ import "github.com/TheLe0/f1-race-analysis/types"
 
 func AnalyzeRace(input []types.RacerInput) {
 
+	var grid []types.Racer
+
+	for _, line := range input {
+
+		var racer types.Racer
+
+		racer.Name = line.Name
+		racer.Number = line.Number
+
+		grid = append(grid, racer)
+	}
 }
---
+++ types/racer.go
@@ -0,0 +1,11 @@
+package types
+
+type Racer struct {
+	Name         string
+	Number       string
+	AverageSpeed uint16
+	Laps         uint8
+	FastestLap   uint16
+	StartTime    uint16
+	FinishTime   uint16
+}
